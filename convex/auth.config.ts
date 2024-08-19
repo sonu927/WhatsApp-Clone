d3434d6dad644211ae865337ca3078c9a4888536
Sonu Raut <sonu16122001@gmail.com>
2024-08-19 11:08:21 +0530

    Webhooks added to set user online,offline session controls

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://simple-ghoul-84.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+export default authConfig; 
