00778153312d58959abcfda603c4400cb9cafa7a
Sonu Raut <sonu16122001@gmail.com>
2024-08-20 23:18:03 +0530

    get real conversation feature added

--- convex/schema.ts
+++ convex/schema.ts
@@ -16,5 +16,12 @@ export default defineSchema({
         groupName: v.optional(v.string()),
         groupImage: v.optional(v.string()),
         admin: v.optional(v.id("users")),
-    })
+    }),
+
+    messages: defineTable({
+        conversation: v.id("conversations"),
+        sender: v.string(), // should be string so it don't throw error in open ai part
+        content: v.string(),
+        messageType: v.union(v.literal("text"),v.literal("image"),v.literal("video")),
+    }).index("by_conversation",["conversation"]),
 })
